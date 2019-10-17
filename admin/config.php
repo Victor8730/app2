100faa83770e785e449429deb6d95b89ac6c052c
Victor <victor8730@ukr.net>
2019-10-17 08:46:47 +0300

    installation done

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cub.loc/admin/');
+define('HTTP_CATALOG', 'http://cub.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cub.loc/admin/');
+define('HTTPS_CATALOG', 'http://cub.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/cubometr/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/cubometr/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/cubometr/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/cubometr/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/cubometr/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/cubometr/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/cubometr/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/cubometr/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/cubometr/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/cubometr/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/cubometr/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/cubometr/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '21238789');
+define('DB_DATABASE', 'cub19');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'cb19_');
--- install/view/template/common/footer.tpl
+++
@@ -1,5 +0,0 @@
-<footer>
-  <div class="container"><a href="http://www.opencart.pro" target="_blank"><?php echo $text_project; ?></a>|<a href="http://opencart.pro/wiki/" target="_blank"><?php echo $text_documentation; ?></a>|<a href="http://forum.opencart.pro" target="_blank"><?php echo $text_support; ?></a><br />
-    </div>
-</footer>
-</body></html>
